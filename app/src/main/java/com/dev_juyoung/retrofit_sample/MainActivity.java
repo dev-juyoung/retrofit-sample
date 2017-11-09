fd06043a073970fbbeee3cc780064d2ad61cf089
dev.juyoung <dev.juyoung@gmail.com>
2017-11-09 14:32:02 +0900

    * API 통신 로직 별도의 Class로 분리.

--- app/src/main/java/com/dev_juyoung/retrofit_sample/MainActivity.java
+++ app/src/main/java/com/dev_juyoung/retrofit_sample/MainActivity.java
@@ -1,19 +1,9 @@
 package com.dev_juyoung.retrofit_sample;
 
 import android.os.Bundle;
-import android.util.Log;
 
 import com.dev_juyoung.retrofit_sample.base.BaseActivity;
-import com.dev_juyoung.retrofit_sample.data.store.SearchInfo;
-import com.dev_juyoung.retrofit_sample.network.GithubService;
-import com.dev_juyoung.retrofit_sample.network.ServiceGenerator;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import com.dev_juyoung.retrofit_sample.data.GithubData;
 
 public class MainActivity extends BaseActivity {
     private static final String TAG = "MainActivity";
@@ -23,25 +13,6 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Map<String, String> queries = new HashMap<>();
-        queries.put("q", "android");
-        queries.put("sort", "stars");
-
-        GithubService service = ServiceGenerator.createService(GithubService.class);
-
-        Call<SearchInfo> request = service.getMostStarsRepositories(queries);
-        request.enqueue(new Callback<SearchInfo>() {
-            @Override
-            public void onResponse(Call<SearchInfo> call, Response<SearchInfo> response) {
-                if (response.isSuccessful()) {
-                    Log.i(TAG, "onResponse(): " + response.body().toString());
-                }
-            }
-
-            @Override
-            public void onFailure(Call<SearchInfo> call, Throwable t) {
-                Log.e(TAG, "onFailure(): " + t.getMessage());
-            }
-        });
+        GithubData.getInstance().getRepositories();
     }
 }
---
+++ app/src/main/java/com/dev_juyoung/retrofit_sample/data/GithubData.java
@@ -0,0 +1,58 @@
+package com.dev_juyoung.retrofit_sample.data;
+
+import android.util.Log;
+
+import com.dev_juyoung.retrofit_sample.data.store.SearchInfo;
+import com.dev_juyoung.retrofit_sample.network.GithubService;
+import com.dev_juyoung.retrofit_sample.network.ServiceGenerator;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+/**
+ * Created by juyounglee on 2017. 11. 9..
+ */
+
+public class GithubData {
+    private static final String TAG = "GithubData";
+
+    private static GithubData instance;
+
+    public static GithubData getInstance() {
+        if (instance == null) {
+            instance = new GithubData();
+        }
+
+        return instance;
+    }
+
+    public void getRepositories() {
+        // 검색 시 사용할 queiry 생성.
+        Map<String, String> quereis = new HashMap<>();
+        quereis.put("q", "android");
+        quereis.put("sort", "stars");
+
+        // REST 클라이언트 생성.
+        GithubService service = ServiceGenerator.createService(GithubService.class);
+
+        // API 요청.
+        Call<SearchInfo> request = service.getMostStarsRepositories(quereis);
+        request.enqueue(new Callback<SearchInfo>() {
+            @Override
+            public void onResponse(Call<SearchInfo> call, Response<SearchInfo> response) {
+                if (response.isSuccessful()) {
+                    Log.i(TAG, "onResponse(): " + response.body());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<SearchInfo> call, Throwable t) {
+                Log.e(TAG, "onFailure(): " + t.getMessage());
+            }
+        });
+    }
+}
